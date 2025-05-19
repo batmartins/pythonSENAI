8c5d05fd5cb302523ae04dac2bb9fd4f50d216ce
batmartins <hsmheitor@gmail.com>
2025-05-19 10:31:20 -0300

    Delete função directory

--- função/função 3.py
+++
@@ -1,9 +0,0 @@
-print("Calculadora Básica")
-
-def calculo(n1, n2):
-    print("os resultados da adição, subtração, multiplicação e divisão são respectivamentes:",n1+n2, n1-n2, n1*n2, n1/n2)
-
-
-n1 = int(input("digite o primeiro número"))
-n2 = int(input("digite o segundo número"))
-calculo(n1, n2)
